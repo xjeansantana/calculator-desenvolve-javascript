4d1faab2551e95396d874b4955349bf012dfa4d1
xjeansantana <51293417+xjeansantana@users.noreply.github.com>
2022-03-04 18:36:14 -0300

    refatoração do código

--- js/calc.js
+++ js/calc.js
@@ -2,18 +2,15 @@ let operacao = [];
 let operacaoResultado = [];
 
 function telaBaixo(entrada) { //entrada de dados
-
   let visor = document.getElementById("visor");
-
   let entradaVisor = document.getElementById("visor").innerHTML;
   let ultimaEntrada = entradaVisor.substring(entradaVisor.length - 1, entradaVisor.length)
 
-  if (isOperator(ultimaEntrada) && isOperator(entrada)) {
-    apagar();
-  }
-
-  visor.innerHTML = visor.innerHTML + entrada;
-}
+    if (isOperator(ultimaEntrada) && isOperator(entrada)) {
+      apagar();
+    }
+     visor.innerHTML = visor.innerHTML + entrada;
+    }
 
 function isOperator(entrada) {
   return /[+-\/*]/.test(entrada) ? true : false;
@@ -21,47 +18,43 @@ function isOperator(entrada) {
 
 
 function telaCima(entrada) { //mostra a operação no visor de cima
-
   var entradaVisor = document.getElementById("visor").innerHTML;
-  document.getElementById("visorCima").innerHTML = entradaVisor;
-
+    document.getElementById("visorCima").innerHTML = entradaVisor;
 }
 
+//Funções apagar
 function clean() { //limpa todos os campos
   apagarVisor();
-  document.getElementById("visorCima").innerHTML = "";
+    document.getElementById("visorCima").innerHTML = "";
 }
-
-function apagarVisor() { //limpa tudo do visor de baixo que recebe a entrada
-
-  document.getElementById("visor").innerHTML = "";
-}
-
-function apagar() { //limpa a ultima entrada
-  let visor = document.getElementById("visor").innerHTML;
-  document.getElementById("visor").innerHTML = visor.substring(0, visor.length - 1);
-}
-
-function calcular() { //calcula
-
-  let visor = document.getElementById("visor").innerHTML;
-
-  var resultado = eval(visor)
-
-  operacao.push(visor)
-
-  if (visor) {
-    telaCima();
-    document.getElementById('visor').innerHTML = resultado
-    operacaoResultado.push(resultado)
-    return;
+  function apagarVisor() { //limpa tudo do visor de baixo que recebe a entrada
+    document.getElementById("visor").innerHTML = "";
   }
+    function apagar() { //limpa a ultima entrada
+      let visor = document.getElementById("visor").innerHTML;
+       document.getElementById("visor").innerHTML = visor.substring(0, visor.length - 1);
+} //termino funcões apagar
 
-}
 
+// funcão calcular
+function calcular() { //calcula
+  let visor = document.getElementById("visor").innerHTML;
+    var resultado = eval(visor)
+     operacao.push(visor)
+
+      if (visor) {
+        telaCima();
+        document.getElementById('visor').innerHTML = resultado
+        operacaoResultado.push(resultado)
+        historico();
+        return;
+      }
+
+} //fim da função calcular
+
+//historico 
 function historico() {
   for (let i = 0; i < operacao.length; i++) {
     console.log(`${operacao[i]} = ${operacaoResultado[i]}`)
   }
-
 }
